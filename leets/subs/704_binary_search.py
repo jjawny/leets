86337d08c29864d8f67659f9b77e32299a124479
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-09 22:27:24 +1000

    python v of 704 binary search

---
+++ leets/subs/704_binary_search.py
@@ -0,0 +1,25 @@
+import math
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        See TS version for Big O
+
+        #sliding-window #two-pointers #binary-search
+        """
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            halfwayAmountBetweenLeftAndRight = math.floor((right - left) / 2)
+            mid = left + halfwayAmountBetweenLeftAndRight
+            midNum = nums[mid]
+            
+            if target == midNum:
+                return mid
+            elif target < midNum:
+                right = mid - 1
+            elif target > midNum:
+                left = mid + 1
+        
+        return -1
