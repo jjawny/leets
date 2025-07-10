3682372fd255e71cd87e4be5f785cea32623328b
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-10 23:02:31 +1000

    added a backtracking template

---
+++ leets/templates/backtracking_template.py
@@ -0,0 +1,9 @@
+# def backtrack(path, choices):
+#     if end_condition(path):
+#         result.append(path.copy())
+#         return
+#     for choice in choices:
+#         if is_valid(choice, path):
+#             path.append(choice)
+#             backtrack(path, choices)
+#             path.pop()
