10c0b040e27a91266831a0354333756575908f35
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-11 09:15:59 +1000

    monotonic stack template

---
+++ leets/templates/monotonic_stack_template.py
@@ -0,0 +1,8 @@
+# StackItem = tuple[int, int] # (idx, val)
+# stack: List[StackItem] = []
+
+# for currIdx, val in enumerate(arr):
+#     while stack and condition(val, stack[-1]):
+#         prevIdx, val = stack.pop()
+#         # ^ do something w these
+#     stack.append((currIdx, val))
