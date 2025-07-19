87696b4a74306bb14b895166f1cfcc07c267b91b
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-19 23:37:18 +1000

    94. Binary Tree Inorder Traversal

---
+++ leets/subs/dfs/94_binary_tree_inorder_traversal.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+from leets.types.tree_node import TreeNode
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        TIME: O(n) as we visit each node
+        MEMORY: O(n) as we capture all nodes again in the result (and visited)
+
+        ##dfs ##binary-search-tree ##recursion
+        """
+        # Qs is basically just the DFS template (recursion strat)
+        result: List[int] = []
+        visited: List[TreeNode] = []
+
+        def dfs(node: Optional[TreeNode]):
+            if not node or node in visited:
+                return
+            visited.append(node)
+            dfs(node.left)
+            result.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+
+        return result
