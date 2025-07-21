1b856f971d2f9bac4978739b58366d9b4059ae68
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-21 22:58:08 +1000

    26 remove duplicates from sorted array

---
+++ leets/subs/26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        TIME: Always O(n) to iterate once
+        SPACE: Always O(1) as in-place mutation (no copies/extra space of size n used)
+        """
+        # Q is unnecessarily (seems intentionally) confusing...
+        # TLDR we need to:
+        #   - Mutate the nums in place
+        #   - 'shifting' all unique items to the start, rest is ignored
+        #   - Maintain asc order
+        #   - Return the length (k)
+        # Q checks the nums after mutation and the k we return
+        if not nums:
+            return 0
+
+        # next_valid_idx is the next slot
+        #   to shift a newly encountered num into
+        #   this also acts as the total count
+        i, next_valid_idx = 1, 1
+        while i < len(nums):
+            # Bcuz we maintain asc order, we can easily check the prev num for dupes, safe as i starts at 1
+            prev = nums[i - 1]
+            if prev != nums[i]:
+                nums[next_valid_idx] = nums[i]
+                next_valid_idx += 1
+            i += 1
+        return next_valid_idx
