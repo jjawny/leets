0cbe3ce4644eef082d7e876086beae57b72964a9
Johnny <74356539+jjawny@users.noreply.github.com>
2025-08-29 23:55:40 +1000

    big o

--- leets/submissions/1_two_sum.py
+++ leets/submissions/1_two_sum.py
@@ -6,7 +6,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
         See TS version for Big O
-
         ##hashmaps
         """
         cache = {}
--- leets/submissions/3_longest_substring_without_repeating_characters.py
+++ leets/submissions/3_longest_substring_without_repeating_characters.py
@@ -1,37 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        TIME: Always O(n)
-        SPACE: Worst-case O(n) if we encounter no dupes and store all n in seen_chars
+        TIME: O(n)
+        SPACE: Worst-case O(n) if we encounter no dupes and store all of n in seen_chars
         ##two-pointers ##sliding-window
         """
-        # Q screams use a sliding window (2 pointers strat)
-        # Track seen_chars in a hashmap as O(1) ops + ordered in Python 3.7+ (unlike Set)
-        #   only keys are important, value is unused
-        seen_chars = {}
-        max_substring_len, left = 0, 0
+        last_index = {}
+        max_len = 0
+        left = 0
 
-        for right in range(len(s)):
-            curr_char = s[right]
-            if curr_char not in seen_chars:
-                seen_chars[curr_char] = None # Value does not matter, use None for 16B mem
-                continue
-            # By here we encountered a dupe:
-            #   1. Save the current substring length (if it's larger)
-            substring_len = right - left
-            max_substring_len = max(max_substring_len, substring_len)
+        for right, char in enumerate(s):
+            if char in last_index and last_index[char] >= left:
+                left = last_index[char] + 1
+            last_index[char] = right
+            max_len = max(max_len, right - left + 1)
 
-            #   2. 'Heal' aka move the start of the window up/right until
-            #       the dupe char (exclusive - dupe char is the next start)
-            while s[left] != curr_char and left < right:
-                if s[left] in seen_chars:
-                    del seen_chars[s[left]]
-                left += 1
-            
-            # Finally shift the start window past the duplicate char as 0-based index
-            left += 1
-
-        # Handle the final length (loop finished and no dupe was encountered right at the end)
-        final_substring_len = len(s) - left
-        max_substring_len = max(max_substring_len, final_substring_len)
-        return max_substring_len
+        return max_len
